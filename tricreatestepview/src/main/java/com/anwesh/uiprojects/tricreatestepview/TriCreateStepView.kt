160aaa673bf41cd6bf703c924d237c972aee900e
Anwesh43 <anweshthecool0@gmail.com>
2018-07-31 10:50:11 +0530

    adding canvas extension method

--- tricreatestepview/src/main/java/com/anwesh/uiprojects/tricreatestepview/TriCreateStepView.kt
+++ tricreatestepview/src/main/java/com/anwesh/uiprojects/tricreatestepview/TriCreateStepView.kt
@@ -8,9 +8,29 @@ import android.content.Context
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 
-val nodes : Int = 5
+val nodes : Int = 3
+
+fun Canvas.drawTCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / nodes
+    val deg : Float = 360f / nodes
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    val size : Float = gap/3
+    val y : Float = (size / 2) / Math.tan(deg.toDouble() / 2).toFloat()
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#3F51B5")
+    save()
+    translate(i * gap + gap/2 + gap * sc2, h / 2)
+    rotate(deg * i)
+    drawLine(-size/2 * sc1, y, size/2 * sc1, y, paint)
+    restore()
+}
 
 class TriCreateStepView(ctx : Context) : View(ctx) {
 
