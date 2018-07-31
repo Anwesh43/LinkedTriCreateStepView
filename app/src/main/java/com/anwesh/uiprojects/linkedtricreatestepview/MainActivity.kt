85ed9f256bfbad02a4317180ccae395fd33f4f76
Anwesh43 <anweshthecool0@gmail.com>
2018-07-31 11:28:19 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtricreatestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtricreatestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtricreatestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tricreatestepview.TriCreateStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriCreateStepView.create(this)
     }
 }
