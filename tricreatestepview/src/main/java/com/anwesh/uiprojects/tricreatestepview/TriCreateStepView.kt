b260b58bcbabcec675ffe540bfa75a613aaec63d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-31 10:19:33 +0530

    adding TriCreateStepView class

--- tricreatestepview/src/main/java/com/anwesh/uiprojects/tricreatestepview/TriCreateStepView.kt
+++ tricreatestepview/src/main/java/com/anwesh/uiprojects/tricreatestepview/TriCreateStepView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.tricreatestepview
 /**
  * Created by anweshmishra on 31/07/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val nodes : Int = 5
+
+class TriCreateStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
